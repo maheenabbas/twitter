cb851c4bdaf40abe6b7435e5344b7bd57cf882ae
Maheen Naqvi <81388494+maheennaqvi@users.noreply.github.com>
2021-10-04 14:55:36 -0400

    Add files via upload

--- HomeTableViewController.swift
+++ HomeTableViewController.swift
@@ -19,11 +19,16 @@ class HomeTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadTweets()
-        
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
-        tableView.refreshControl = myRefreshControl
+        self.tableView.refreshControl = myRefreshControl
         
     }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+            
+    }
+    
     
     @objc func loadTweets(){
         
@@ -99,6 +104,9 @@ class HomeTableViewController: UITableViewController {
         }
         
         
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
